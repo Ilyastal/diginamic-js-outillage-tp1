4d4a8e87cb29d2dd780f2e1cba6b9405a90a03b9
ilyas <ilyes.tal90@gmail.com>
2019-10-16 18:46:18 +0200

    hello

--- util.js
+++ util.js
@@ -15,8 +15,9 @@ var even = function () {};
 even.prototype.log = function () {
     var tab1 =[];
     for(var i=1; i<=100; i++) {
-        tab1[i] = i*2 ;
-        tab1.push(i);
+        if(i % 2 === 0){
+            tab1.push(i);
+           }
      }
      return tab1;
 }
@@ -28,8 +29,9 @@ var odd = function () {};
 odd.prototype.log = function () {
     var tab2 =[];
     for(var i=0; i<=100; i++) {
-        tab2[i] = i*2 + 1;
-        tab2.push(i);
+        if(i % 2 !== 0){
+            tab2.push(i);
+           }
      }
      return tab2;
 }
